18593c3b290c686140f16b68f6fb6e7b36fc5a0c
lazar <lazarmateiconstantin@gmail.com>
2025-05-12 00:13:58 +0300

    <feat>:<task3 initially combined with task1 but eventually combined with task2 >

--- lib_gameoflife.c
+++ lib_gameoflife.c
@@ -77,6 +77,7 @@ void copy_matr(char *matr, char **cpy_matr, int lin, int col)
     for(i=0; i<lin; i++)
         for(j=0; j<col; j++)
             *((*cpy_matr) + i*col + j) = *(matr + i*col + j) ;
+    
 }
 
 
@@ -196,3 +197,147 @@ void rez_generatie_bonus(char *matr, int col, Node *top)
     
 }
     */
+/*
+void preord_afis(T_Node *root, FILE *fout, int lin, int col)
+{
+    if(root !=NULL)
+    {
+        //scriere_f_out(fout, root->grila, lin, col);
+        preord_afis(root->left, fout, lin, col);
+        preord_afis(root->right, fout, lin, col);
+    }
+}
+*/
+char* rule1(char *matr, int lin, int col, Node **top)
+{
+    Poz v;
+    int i, j;
+    VECIN nr_vecini;
+    char *matr_ctrl, *matr2use;
+    matr_ctrl = NULL;
+    copy_matr(matr, &matr_ctrl, lin, col);
+    copy_matr(matr, &matr2use, lin, col);
+    for(i=0; i<lin; i++)
+        for(j=0; j<col; j++)
+        {
+            v.coloana = j;
+            v.linie = i;
+            nr_vecini = stare_vecini( matr_ctrl, j, i, col, lin);
+            if( *( matr2use + i * col + j ) == 'X' )
+                {
+                    if ( (nr_vecini.vii < 2) || ( nr_vecini.vii > 3 ) ){
+                        *( matr2use + i * col + j ) = '+';
+                        push(top, v);
+                        
+                    }
+                    
+                }
+            else if( (*( matr2use + i * col + j ) == '+') && (nr_vecini.vii == 3) ){
+                *( matr2use + i * col + j ) = 'X';
+                push(top, v);
+            }
+        }
+    free(matr_ctrl);
+    //free(matr);
+    return matr2use;
+}
+
+char* rule2(char *matr, int lin, int col, Node **top)
+{
+    Poz v;
+    int i, j;
+    VECIN nr_vecini;
+    char *matr_ctrl, *matr2use;
+    matr_ctrl = NULL;
+    copy_matr(matr, &matr_ctrl, lin, col);
+    copy_matr(matr, &matr2use, lin, col);
+    for(i=0; i<lin; i++)
+        for(j=0; j<col; j++)
+        {
+            v.coloana = j;
+            v.linie = i;
+            nr_vecini = stare_vecini( matr_ctrl, j, i, col, lin);
+            if( (*( matr2use + i * col + j ) == '+') && (nr_vecini.vii == 2) )
+            {
+                *( matr2use + i * col + j ) = 'X';
+                push(top, v);
+            }
+        }
+    free(matr_ctrl);
+    //free(matr);
+    return matr2use;
+}
+
+void create_succ(T_Node* root, int nr_gen)
+{
+    if(root->generatie == nr_gen)
+        return;
+    root->left = (T_Node*)malloc(sizeof(T_Node));
+    root->right = (T_Node*)malloc(sizeof(T_Node));
+    root->stack = NULL;
+    root->left->left = NULL;
+    root->left->right = NULL;
+    root->right->left = NULL;
+    root->right->right = NULL;
+    root->left->generatie = root->right->generatie = root->generatie+1;
+    create_succ(root->left, nr_gen);
+    create_succ(root->right, nr_gen);
+}
+void preord_rez_gen(T_Node *root, int lin, int col, char *grila)
+{
+    if(root->left)
+    {
+        char *grila1 = NULL;
+        char *grila2 = NULL;
+        
+        grila1 = rule2(grila, lin, col, &root->left->stack);
+        grila2 = rule1(grila, lin, col, &root->right->stack);
+        
+       
+        if (root->generatie>0) 
+        {
+            free(grila);
+        }
+        
+       
+        preord_rez_gen(root->left, lin, col, grila1);
+        preord_rez_gen(root->right, lin, col, grila2);
+    }
+    else if (root->generatie > 0) 
+            free(grila);
+            
+}
+void deleteTree(T_Node** root)
+{
+    if(*root != NULL)
+    {
+        deleteTree(&(*root)->left);
+        deleteTree(&(*root)->right);
+        deleteStack(&(*root)->stack);
+        free(*root);
+        *root = NULL;
+    }
+}
+
+void changeNprint(FILE *f,char *grila, int lin, int col, T_Node *root)
+{
+    if(root ==NULL) return;
+    //invertStack(&root->stack);
+    while(!isEmpty(root->stack))
+    {
+        Poz v = pop(&root->stack);
+        if(*(grila+ v.linie *col + v.coloana) == 'X')
+            *(grila+ v.linie *col + v.coloana) = '+';
+        else *(grila+ v.linie *col + v.coloana) = 'X';
+    }
+    scriere_f_out(f, grila, lin, col);
+    char *grila1 = NULL;
+    char *grila2 = NULL;
+    copy_matr(grila, &grila1, lin, col);
+    copy_matr(grila, &grila2, lin, col);
+    changeNprint(f, grila1, lin, col, root->left);
+    changeNprint(f, grila2, lin, col, root->right);
+    free(grila1);
+    free(grila2);
+
+}
--- lib_gameoflife.h
+++ lib_gameoflife.h
@@ -32,6 +32,16 @@ struct stack_node{
 };
 typedef struct stack_node stackNode;
 
+struct tree{
+    //char *grila;
+    Node *stack;
+    int generatie;
+    struct tree *left, *right;
+};
+typedef struct tree T_Node;
+
+
+
 
 void citire_f_in(FILE *f_in, int *task, int *col, int *lin, int *gen, char **matr);
 void scriere_f_out( FILE *f_out, char *matr, int lin, int col);
@@ -46,5 +56,11 @@ void deleteStack(Node** top);
 void fprintStack(Node* top, FILE* f_out);
 void printStack(Node* top);
 void invertStack(Node**top);
-
+void preord_afis(T_Node *root, FILE *fout, int lin, int col);
+char* rule1(char *matr, int lin, int col, Node **top);
+char* rule2(char *matr, int lin, int col, Node **top);
+void create_succ(T_Node* root, int nr_gen);
+void preord_rez_gen(T_Node *root, int lin, int col, char *grila);
+void deleteTree(T_Node** root);
+void changeNprint(FILE *f,char *grila, int lin, int col, T_Node *root);
 
